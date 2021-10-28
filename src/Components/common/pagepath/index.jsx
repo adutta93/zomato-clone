3b33d9ff057ffb807ee3559ab8a351918cdad6af
akash-dutta-au7 <dutta.akash1993official@gmail.com>
2021-10-28 20:46:08 +0530

    page path added

---
+++ src/Components/common/pagepath/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./pagepath.css";
+const Pagepath = () => {
+  return (
+    <div className="max-width pagepath">
+      <span>Home</span> / <span>India</span> /{" "}
+      <span className="city-name">Kolkata resturants</span>
+    </div>
+  );
+};
+
+export default Pagepath;
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -5,12 +5,13 @@ import Footer from "../../Components/common/Footer";
 import Delivery from "../../Components/Delivery";
 import DiningOut from "../../Components/DiningOut";
 import NightLife from "../../Components/NightLife";
-
+import Pagepath from "../../Components/common/pagepath";
 const Home = () => {
   const [activeTab, setActiveTab] = useState("Delivery");
   return (
     <div>
       <Header />
+      <Pagepath />
       <TabOptions activeTab={activeTab} setActiveTab={setActiveTab} />
       {/* change pages */}
       {getCorrectScreen(activeTab)}
